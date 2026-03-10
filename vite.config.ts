f8288cfda907d904a89a315c83427ba3f204912b
Shaurya Baruah <baruahs@umich.edu>
2026-03-10 17:28:07 -0400

    fixing issue after dns

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/remira-insight-launch/",
+  //base: "/remira-insight-launch/",
   server: {
     host: "::",
     port: 8080,

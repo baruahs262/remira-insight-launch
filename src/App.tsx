b3d4437e5cc30abe0eea18d367854e995dcf60b0
Shaurya Baruah <baruahs@umich.edu>
2026-03-10 17:34:15 -0400

    fixed other file too

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/remira-insight-launch">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
